c1499f87ca9938dd3772955d763ec5c8f8a1bf96
Allan <allanblamano@gmail.com>
2023-01-12 19:12:07 -0300

    Centralizadas as partes da tela da home no componente home-page (assim excluindo a necessidade dos componentes custom-grid e custom-card)

--- needhelp/src/app/app.module.ts
+++ needhelp/src/app/app.module.ts
@@ -3,32 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 
 import { AppComponent } from './app.component';
 import { HeaderBarComponent } from './header-bar/header-bar.component';
 import { GreeterAreaComponent } from './greeter-area/greeter-area.component';
-import { CustomGridComponent } from './custom-grid/custom-grid.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { TutorialPageComponent } from './tutorial-page/tutorial-page.component';
-import { CustomCardComponent } from './custom-card/custom-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderBarComponent,
     GreeterAreaComponent,
-    CustomGridComponent,
     HomePageComponent,
-    TutorialPageComponent,
-    CustomCardComponent
+    TutorialPageComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    MatCardModule
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
